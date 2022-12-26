86aecf31547a73d374ab86f013dd86f654fda7e5
recoveryMonster <fengsongting@gmail.com>
2022-12-26 23:38:56 +0800

    抽离 storestore

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,10 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
 import { router } from "./router";
-import { createPinia } from "pinia";
-import { $http } from "./service";
-$http.post({
-  url: "/role/list",
-  data: {
-    offset: 0,
-    size: 1000,
-  },
-});
+import { store } from "./store";
 
-const pinia = createPinia();
 const app = createApp(App);
 
 app.use(router);
-app.use(pinia);
+app.use(store);
 app.mount("#app");
